79dc48054743d7bd7bfd4fa63c94447f84f04d2e
Virjanand Panday <virjanand@virjanand.com>
2022-07-16 08:24:41 +0200

    Replace Function with Supplier

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,23 +1,22 @@
 package fizzbuzz;
 
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, result1 -> {
+        return IntStream.rangeClosed(from, until).mapToObj(number -> toFizzBuzz(number, () -> {
             if (number % 3 == 0) {
-                result1 = "Fizz";
+                return "Fizz";
             }
-            return result1;
+            return "";
         })).collect(Collectors.toList());
     }
 
-    private String toFizzBuzz(int number, Function<String, String> fizzRule) {
-        String result = "";
-        result = fizzRule.apply(result);
+    private String toFizzBuzz(int number, Supplier<String> fizzRule) {
+        String result = fizzRule.get();
         if (number % 5 == 0) {
             result += "Buzz";
         }
